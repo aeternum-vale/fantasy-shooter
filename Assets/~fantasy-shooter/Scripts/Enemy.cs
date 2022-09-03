4aa3d045ffd1ed95f0dd8f6e74807255e526bcb6
Artur Dedyulya <aeternum-vale@ya.ru>
2022-09-03 21:17:35 +0300

    implement fsm for enemy

--- Assets/~fantasy-shooter/Scripts/Enemy.cs
+++ Assets/~fantasy-shooter/Scripts/Enemy.cs
@@ -1,8 +1,16 @@
+using MonsterLove.StateMachine;
 using UnityEngine;
 
 using static FantasyShooter.Constants;
+
 public class Enemy : MonoBehaviour
 {
+    private enum States
+    {
+        Move,
+        Attack,
+        Death
+    }
 
     [SerializeField] private Animator _animator;
     [SerializeField] Transform _target;
@@ -11,46 +19,91 @@ public class Enemy : MonoBehaviour
     [Range(0, 0.99f)]
     [SerializeField] private float _rotateSmoothness = 0.5f;
     [SerializeField] private float _speed;
+    [SerializeField] private float _attackDistance;
 
-    private bool _isKilled = false;
+    private StateMachine<States> _fsm;
+
+    private bool _isDead = false;
     private Vector3 _targetPoint;
 
-    private int _killTriggerID;
+    private int _deathTriggerID;
     private int _deathVariantID;
+    private int _attackFlagID;
 
+    private const string MoveState = "Moving";
     private const int DeathVariantsCount = 4;
 
     private void Awake()
     {
+        _fsm = new StateMachine<States>(this);
+        _fsm.ChangeState(States.Move);
+
         AssignAnimationIDs();
+
         _animator.SetInteger(_deathVariantID, Random.Range(1, DeathVariantsCount + 1));
     }
 
     private void AssignAnimationIDs()
     {
-        _killTriggerID = Animator.StringToHash("Kill");
+        _deathTriggerID = Animator.StringToHash("DeathTrigger");
+        _attackFlagID = Animator.StringToHash("AttackFlag");
         _deathVariantID = Animator.StringToHash("DeathVariant");
     }
 
     private void Update()
     {
-        if (_isKilled) return;
-
-        _targetPoint = Vector3.Slerp(_targetPoint, _target.position, (1f - _rotateSmoothness) * DeltaTimeCorrection);
-        transform.LookAt(_targetPoint);
-
-
-        _rigidBody.MovePosition(transform.position + _speed * Time.deltaTime * transform.forward);
+        _fsm.Driver.Update.Invoke();
     }
 
     public void Kill()
     {
-        _collider.enabled = false;
-        _rigidBody.isKinematic = true;
-
-        _isKilled = true;
-        _animator.SetTrigger(_killTriggerID);
+        _fsm.ChangeState(States.Death);
     }
 
+    private void RotateTowardTarget()
+    {
+        _targetPoint = Vector3.Slerp(_targetPoint, _target.position, (1f - _rotateSmoothness) * DeltaTimeCorrection);
+        transform.LookAt(_targetPoint);
+    }
+
+    #region FSM
+
+    private void Move_Enter()
+    {
+        _animator.SetBool(_attackFlagID, false);
+
+        _animator.Play(MoveState, -1, Random.Range(0f, 1f));
+    }
+
+    private void Move_Update()
+    {
+        RotateTowardTarget();
+
+        _rigidBody.MovePosition(transform.position + _speed * Time.deltaTime * transform.forward);
+        if (Vector3.Distance(transform.position, _target.position) <= _attackDistance)
+            _fsm.ChangeState(States.Attack);
+    }
+
+    private void Death_Enter()
+    {
+        _isDead = true;
+        _collider.enabled = false;
+        _rigidBody.isKinematic = true;
+        _animator.SetTrigger(_deathTriggerID);
+    }
+
+    private void Attack_Enter()
+    {
+        _animator.SetBool(_attackFlagID, true);
+    }
+
+    private void Attack_Update()
+    {
+        RotateTowardTarget();
+        if (Vector3.Distance(transform.position, _target.position) > _attackDistance)
+            _fsm.ChangeState(States.Move);
+    }
+
+    #endregion
 
 }
